34afabb1257bceb072d403e8e6af7217f90ffd16
Yajurmani Sharma <37212309+ecchi-yajur@users.noreply.github.com>
2020-10-18 20:00:28 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,5 +1,7 @@
 import discord
 
+helpstring = "Works for both anime and manga.\n 1. Use #anime desc <anime_name> to get the synopsis of an anime.\n 2. Use #anime info <anime_name> to get extended anime information.\n 3. Use #anime recommend <anime_name> to get similar anime recommendations.\n 4. Use #anime trailer <anime_name> to get trailers for the anime.\n 4. Use #anime song <anime_name> to get list of anime openings and endings.\n 5. Use #anime play <song_name> to play the exact song_name as on Youtube.\n 6. Use #anime search <anime_name> to search for an anime (currently being worked on as there are issues).\n We will try our best at maintaining these features and adding more interesting ones, So stay tuned !!!"
+
 def easyembed(bot , title , description , imgurl = 'none' , thumbnailurl = 'none'):
     embed = discord.Embed()
     embed.title = title
